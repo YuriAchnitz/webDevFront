71f979b4dfeffeef69404c6a1a08ee74fbdeadbb
Vinicius Ferreira <vini.ferr33@gmail.com>
2022-06-04 22:53:39 -0300

    fixed navbar weird

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,14 +4,40 @@ import { MdOutlineMenuBook } from "react-icons/md";
 import { RiShoppingCart2Fill } from "react-icons/ri";
 import { RiShoppingBag3Fill } from "react-icons/ri";
 
+import { useNavigate } from 'react-router-dom';
+
 function SideBar() {
+
+    const navigate = useNavigate();
+
+    const logout = (event) => {
+        event.preventDefault();
+        localStorage.clear();
+        navigate('/');
+    };
+
+    const cardapio = (event) => {
+        event.preventDefault();
+        navigate('/cardapio');
+    };
+
+    const pedidos = (event) => {
+        event.preventDefault();
+        navigate('/orders');
+    };
+
+    const carrinho = (event) => {
+        event.preventDefault();
+        navigate('/cart');
+    };
+
     return (
         <div className="sideBarCcontainer">
-            <button className="returnButton" onClick="pass"> <FaArrowLeft /> </button>
+            <button className="returnButton" onClick={logout}> <FaArrowLeft /> </button>
             <ProfileBox />
-            <button className="sideBarButton" onClick="pass"> <MdOutlineMenuBook /> <a href="/cardapio">Cardápio</a> </button>
-            <button className="sideBarButton" onClick="pass"> <RiShoppingCart2Fill /> <a href="/cart">Carrinho</a> </button>
-            <button className="sideBarButton" onClick="pass"> <RiShoppingBag3Fill /> <a href="/orders">Pedidos</a> </button>
+            <button className="sideBarButton" onClick={cardapio}> <MdOutlineMenuBook /> Cardápio </button>
+            <button className="sideBarButton" onClick={carrinho}> <RiShoppingCart2Fill /> Carrinho </button>
+            <button className="sideBarButton" onClick={pedidos}> <RiShoppingBag3Fill /> Pedidos </button>
         </div>
     )
 }
